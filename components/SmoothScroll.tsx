d27c843eb133da519f3a5c3538ff9425ecdaadce
Mangalam Kumar <mangalamkumar2002@gmail.com>
2024-05-22 23:09:57 +0530

    init

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import { ReactLenis } from "@studio-freight/react-lenis";
+// import { ReactLenis } from "@studio-freight/react-lenis";
 import React from "react";
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
     return (
-        <ReactLenis root>
-            {children}
-        </ReactLenis>
+        // <ReactLenis root>
+        { children }
+        // </ReactLenis>
     )
 }
